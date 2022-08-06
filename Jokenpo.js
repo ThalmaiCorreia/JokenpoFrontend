31ce128af16b2e63573bb4259a6a832b97515a22
Thalmai Correia <thalmaicorreia7@gmail.com>
2022-08-06 00:22:00 -0300

    Implementou estilização com CSS e otimizou lógica no JS

--- Jokenpo.js
+++ Jokenpo.js
@@ -1,35 +1,66 @@
+var jogadorEscolha = 0;
+var computadorEscolha = 0;
+var pontosJogador = 0;
+var pontosComputador = 0;
 
-function jogar() {
-    if (document.getElementById("pedra").checked == false && document.getElementById("papel").checked == false && document.getElementById("tesoura").checked == false) {
-        alert("Selecione uma opção");
-    } else {
-        var sorteio = Math.floor(Math.random() * 3);
-        switch (sorteio) {
-            case 0:
-                document.getElementById("robo").src = "pedra robo .png";
-                break;
-            case 1:
-                document.getElementById("robo").src = "papelrobo.png";
-                break;
-            case 2:
-                document.getElementById("robo").src = "tesoura robo.png";
-                break;
-        }
-        if ((document.getElementById("pedra").checked == true && sorteio == 0) || (document.getElementById("papel").checked == true && sorteio == 1) || (document.getElementById("tesoura").checked == true && sorteio == 2)) {
-            document.getElementById("placar").innerHTML = "Empate";
-        }
-        else if ((document.getElementById("pedra").checked == true && sorteio == 2) || (document.getElementById("papel").checked == true && sorteio == 0) || (document.getElementById("tesoura").checked == true && sorteio == 1)) {
-            document.getElementById("placar").innerHTML = "Jogador Venceu";
-        }
-        else {
-            document.getElementById("placar").innerHTML = "Computador Venceu";
-        }
+function jogar(escolha) {
+    jogadorEscolha = escolha;
+
+    computadorEscolha = Math.floor(Math.random() * 3);
 
 
+    if ((jogadorEscolha == 0 && computadorEscolha == 0) || (jogadorEscolha == 1 && computadorEscolha == 1) || (jogadorEscolha == 2 && computadorEscolha == 2)) {
+        document.getElementById("mensagens").innerHTML = "Empate !";
+        pontosComputador += 1;
+        pontosJogador += 1;
+        document.getElementById("jogador-pontos").innerHTML = pontosJogador;
+        document.getElementById("computador-pontos").innerHTML = pontosComputador;
     }
+
+    else if ((jogadorEscolha == 0 && computadorEscolha == 2) || (jogadorEscolha == 1 && computadorEscolha == 0) || (jogadorEscolha == 2 && computadorEscolha == 1)) {
+        document.getElementById("mensagens").innerHTML = "Jogador Ganhou !";
+        pontosJogador += 2;
+        document.getElementById("jogador-pontos").innerHTML = pontosJogador;
+    }
+
+    else {
+        document.getElementById("mensagens").innerHTML = "Computador Ganhou !";
+        pontosComputador += 2;
+        document.getElementById("computador-pontos").innerHTML = pontosComputador;
+    }
+
+    document.getElementById("jogador-escolha-0").classList.remove('selecionado');
+    document.getElementById("jogador-escolha-1").classList.remove('selecionado');
+    document.getElementById("jogador-escolha-2").classList.remove('selecionado');
+
+    document.getElementById("computador-escolha-0").classList.remove('selecionado');
+    document.getElementById("computador-escolha-1").classList.remove('selecionado');
+    document.getElementById("computador-escolha-2").classList.remove('selecionado');
+
+    document.getElementById("jogador-escolha-" + jogadorEscolha).classList.add('selecionado');
+    document.getElementById("computador-escolha-" + computadorEscolha).classList.add('selecionado');
+
 }
 
 function resetar() {
-    document.getElementById("robo").src = "robo .png";
-    document.getElementById("placar").innerHTML = "";
+
+    document.getElementById("mensagens").innerHTML = "Novo Jogo !";
+
+
+    document.getElementById("jogador-escolha-0").classList.remove('selecionado');
+    document.getElementById("jogador-escolha-1").classList.remove('selecionado');
+    document.getElementById("jogador-escolha-2").classList.remove('selecionado');
+
+    document.getElementById("computador-escolha-0").classList.remove('selecionado');
+    document.getElementById("computador-escolha-1").classList.remove('selecionado');
+    document.getElementById("computador-escolha-2").classList.remove('selecionado');
+
+    pontosComputador = 0;
+    pontosJogador = 0;
+
+    document.getElementById("jogador-pontos").innerHTML = pontosJogador;
+    document.getElementById("computador-pontos").innerHTML = pontosComputador;
+
+
 }
+
